a734c0aa7901d3f24f0ac2abb543b870b5c4111a
Ray Nham <ray.nham@octopus.com>
2021-02-17 16:20:02 +1100

    Added: test config file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import configData from "./config.json";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Ray Nham - Octopus Learning
         </p>
+        <p>{configData.greeting}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
